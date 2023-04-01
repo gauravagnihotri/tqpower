1f7ddf4413378464261ae19beee96954d67be5cb
“gauravagnihotri” <gaagniho@mtu.edu>
2023-04-01 00:27:38 -0400

    Updated Widgets

--- tqpwer1.py
+++ tqpwer1.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 # Define constants
 # wheel_radius = 0.3  # meters
-vehicle_mass = 3500  # kg
+# vehicle_mass = 3500  # kg
 rolling_resistance_coefficient = 0.01
 air_density = 1.2  # kg/m^3
 drag_coefficient = 0.5
@@ -57,26 +58,124 @@ def app():
     st.title(
         "Kevin and Evan's Dilemma - Vehicle Torque, Acceleration, and Speed Calculator"
     )
-    st.table(
+    # st.table(
+    #     {
+    #         "Vehicle Mass": "3500 kg (~7700 lbs))",
+    #         "Rolling Resistance Coefficient": "0.01",
+    #         "Air Density": "1.2 kg/m^3",
+    #         "Drag Coefficient": "0.5",
+    #         "Frontal Area": "3.2 m^2",
+    #     }
+    # )
+    # generate above table as dataframe
+    df = pd.DataFrame(
         {
-            "Vehicle Mass": "3500 kg (~7700 lbs))",
             "Rolling Resistance Coefficient": "0.01",
             "Air Density": "1.2 kg/m^3",
             "Drag Coefficient": "0.5",
             "Frontal Area": "3.2 m^2",
-        }
+        },
+        index=[0],
     )
+    # rename index as "Parameter"
+    df.index = ["Parameters"]
+    # display table
+    st.write("Assumptions")
+    st.dataframe(df, use_container_width=True)
+    # st.markdown("""---""")
     st.title("Input Parameters")
     st.markdown("""---""")
     # Get inputs from user
-    wheel_radius = st.number_input("Enter wheel radius (m):", value=0.43, step=0.01)
+    # wheel_radius = st.number_input("Enter wheel radius (m):", value=0.43, step=0.01)
+    vehicle_mass = st.slider(
+        "Enter Vehicle Mass (lbs):",
+        min_value=0.0,
+        max_value=10000.0,
+        value=7700.0,
+        step=10.0,
+    )
+    # convert vehicle mass to kg
+    vehicle_mass = vehicle_mass * 0.453592
+    wheel_radius = st.slider(
+        "Enter Wheel Radius (m):",
+        min_value=0.0,
+        max_value=1.0,
+        value=0.43,
+        step=0.01,
+    )
     st.write("Ram 1500 wheel radius is 0.43 m for 275/55R20 tires")
-    engine_torque = st.number_input("Enter engine torque (Nm):", value=200.0, step=10.0)
-    st.write("Ram 1500 engine torque is 200 bcoz MDS")
+
+    # engine_torque = st.number_input("Enter engine torque (Nm):", value=200.0, step=10.0)
+
+    # st.write("Ram 1500 engine torque is 200 bcoz MDS")
     # gear_ratio = st.number_input("Enter gear ratio:", value=1.67, step=0.01)
 
-    selected_gear = st.selectbox(
-        "Select gear:", ("1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th")
+    # selected_gear = st.selectbox(
+    #     "Select gear:",
+    #     ("1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"),
+    #     index=3,
+    # )
+    # if selected_gear == "1st":
+    #     gear_ratio = 4.71
+    # elif selected_gear == "2nd":
+    #     gear_ratio = 3.14
+    # elif selected_gear == "3rd":
+    #     gear_ratio = 2.10
+    # elif selected_gear == "4th":
+    #     gear_ratio = 1.67
+    # elif selected_gear == "5th":
+    #     gear_ratio = 1.29
+    # elif selected_gear == "6th":
+    #     gear_ratio = 1.00
+    # elif selected_gear == "7th":
+    #     gear_ratio = 0.84
+    # elif selected_gear == "8th":
+    #     gear_ratio = 0.67
+
+    # engine_speed = st.number_input("Enter engine speed (RPM):", value=2000, step=100)
+    engine_speed = st.slider(
+        "Enter Engine Speed (RPM):",
+        min_value=0.0,
+        max_value=7000.0,
+        value=2000.0,
+        step=100.0,
+    )
+    engine_torque = st.slider(
+        "Enter Engine Torque (Nm):",
+        min_value=0.0,
+        max_value=550.0,
+        value=200.0,
+        step=10.0,
+    )
+
+    # st.write(
+    #     "ZF 8HP70 Gear Ratios: 1st: 4.71, 2nd: 3.14, 3rd: 2.10, 4th: 1.67, 5th: 1.29, 6th: 1.00, 7th: 0.84, 8th: 0.67"
+    # )
+    # generate gear dataframe columnwise
+    gear_ratios = pd.DataFrame(
+        {
+            "1st": [4.71],
+            "2nd": [3.14],
+            "3rd": [2.10],
+            "4th": [1.67],
+            "5th": [1.29],
+            "6th": [1.00],
+            "7th": [0.84],
+            "8th": [0.67],
+        }
+    )
+    # rename gear_ratios first row to gear
+    gear_ratios = gear_ratios.rename(index={0: "Reduction Ratio"})
+    # rename header row to 'gear'
+    gear_ratios.index.name = "Gear"
+
+    st.dataframe(gear_ratios, use_container_width=True)
+    ###
+    selected_gear = st.radio(
+        "Select gear:",
+        ("1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"),
+        index=3,
+        horizontal=True,
     )
     if selected_gear == "1st":
         gear_ratio = 4.71
@@ -94,14 +193,11 @@ def app():
         gear_ratio = 0.84
     elif selected_gear == "8th":
         gear_ratio = 0.67
+    # axle_ratio = st.number_input("Enter axle ratio:", value=3.92, step=0.01)
+    axle_ratio = st.select_slider(
+        "Select axle ratio:", options=[3.21, 3.55, 3.92, 4.10], value=3.92
+    )
 
-    st.write(
-        "ZF 8HP70 Gear Ratios: 1st: 4.71, 2nd: 3.14, 3rd: 2.10, 4th: 1.67, 5th: 1.29, 6th: 1.00, 7th: 0.84, 8th: 0.67"
-    )
-    axle_ratio = st.number_input("Enter axle ratio:", value=3.92, step=0.01)
-    engine_speed = st.number_input(
-        "Enter engine speed (RPM):", value=2000.0, step=100.0
-    )
     # Calculate results
     torque_at_wheel, acceleration, vehicle_speed_mph, force_at_wheel = calculate(
         vehicle_mass,
@@ -117,9 +213,11 @@ def app():
     )
     st.title("Output")
     st.markdown("""---""")
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2 = st.columns(2)
     col1.metric("Torque at wheel:", "{:.2f} Nm".format(torque_at_wheel))
     col2.metric("Force at wheel:", "{:.2f} Nm".format(force_at_wheel))
+    st.markdown("""---""")
+    col3, col4 = st.columns(2)
     col3.metric("Acceleration:", "{:.2f} m/s^2".format(acceleration))
     col4.metric("Vehicle speed:", "{:.2f} mph".format(vehicle_speed_mph))
 
